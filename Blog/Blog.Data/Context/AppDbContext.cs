0acd40e62136a8420a922489cf7ef3f644b7867f
muazerdemyigit <muazerdemyigit@hotmail.com>
2023-09-06 08:27:19 +0300

    Created the DbContext object and established the database connection. Retrieved the ConnectionString from the appsettings file.

---
+++ Blog/Blog.Data/Context/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Blog.Entity.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Data.Context
+{
+    public class AppDbContext : DbContext
+    {
+        protected AppDbContext()
+        {
+        }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Image> Images { get; set; }
+    }
+}
