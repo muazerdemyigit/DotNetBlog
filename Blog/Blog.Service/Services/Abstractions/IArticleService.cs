cae83bda9c83e3a7ee135e9eed2608240478a87a
muazerdemyigit <muazerdemyigit@hotmail.com>
2023-09-11 07:28:15 +0300

    Completed listing operations in relational tables and established DTO relationships using AutoMapper.

--- Blog/Blog.Service/Services/Abstractions/IArticleService.cs
+++ Blog/Blog.Service/Services/Abstractions/IArticleService.cs
@@ -4,6 +4,6 @@ namespace Blog.Service.Services.Abstractions
 {
     public interface IArticleService
     {
-        Task<List<ArticleDto>> GetAllArticlesAsync();
+        Task<List<ArticleDto>> GetAllArticlesWithCategoryNonDeletedAsync();
     }
 }
--- Blog/Blog.Web/Areas/Admin/Controllers/ArticleController.cs
+++ Blog/Blog.Web/Areas/Admin/Controllers/ArticleController.cs
@@ -1,16 +1,14 @@
 ﻿using Blog.Service.Services.Abstractions;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Web.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    [Authorize]
-    public class HomeController : Controller
+    public class ArticleController : Controller
     {
         private readonly IArticleService articleService;
 
-        public HomeController(IArticleService articleService)
+        public ArticleController(IArticleService articleService)
         {
             this.articleService = articleService;
         }
