cc496626ec853bab478d349cc907e9e8edf5b41b
muazerdemyigit <muazerdemyigit@hotmail.com>
2023-09-13 06:06:06 +0300

    Utilized Fluent Validation and made it more Turkish-friendly.

--- Blog/Blog.Service/Extensions/ServiceLayerExtensions.cs
+++ Blog/Blog.Service/Extensions/ServiceLayerExtensions.cs
@@ -1,6 +1,9 @@
-﻿using Blog.Service.Services.Abstractions;
+﻿using Blog.Service.FluentValidations;
+using Blog.Service.Services.Abstractions;
 using Blog.Service.Services.Concrete;
+using FluentValidation.AspNetCore;
 using Microsoft.Extensions.DependencyInjection;
+using System.Globalization;
 using System.Reflection;
 
 namespace Blog.Service.Extensions
@@ -15,6 +18,13 @@ namespace Blog.Service.Extensions
             services.AddScoped<ICategoryService, CategoryService>();
 
             services.AddAutoMapper(assembly);
+
+            services.AddControllersWithViews().AddFluentValidation(opt =>
+            {
+                opt.RegisterValidatorsFromAssemblyContaining<ArticleValidator>();
+                opt.DisableDataAnnotationsValidation = true;
+                opt.ValidatorOptions.LanguageManager.Culture = new CultureInfo("tr");
+            });
             return services;
         }
     }
--- Blog/Blog.Web/Areas/Admin/Controllers/ArticleController.cs
+++ Blog/Blog.Web/Areas/Admin/Controllers/ArticleController.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
 using Blog.Entity.DTOs.Article;
+using Blog.Entity.Entities;
+using Blog.Service.Extensions;
 using Blog.Service.Services.Abstractions;
+using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Web.Areas.Admin.Controllers
@@ -11,12 +14,14 @@ namespace Blog.Web.Areas.Admin.Controllers
         private readonly IArticleService articleService;
         private readonly ICategoryService categoryService;
         private readonly IMapper mapper;
+        private readonly IValidator<Article> validator;
 
-        public ArticleController(IArticleService articleService, ICategoryService categoryService, IMapper mapper)
+        public ArticleController(IArticleService articleService, ICategoryService categoryService, IMapper mapper, IValidator<Article> validator)
         {
             this.articleService = articleService;
             this.categoryService = categoryService;
             this.mapper = mapper;
+            this.validator = validator;
         }
         public async Task<IActionResult> Index()
         {
@@ -32,11 +37,24 @@ namespace Blog.Web.Areas.Admin.Controllers
         [HttpPost]
         public async Task<IActionResult> Add(ArticleAddDto articleAddDto)
         {
-            await articleService.CrateArticleAsync(articleAddDto);
-            RedirectToAction("Index", "Article", new { Area = "Admin" });
+            var map = mapper.Map<Article>(articleAddDto);
+            var result = await validator.ValidateAsync(map);
+
+            if (result.IsValid)
+            {
+                await articleService.CrateArticleAsync(articleAddDto);
+                return RedirectToAction("Index", "Article", new { Area = "Admin" });
+            }
+            else
+            {
+                result.AddToModelState(this.ModelState);
+
+
+            }
 
             var categories = await categoryService.GetAllCategoriesNonDeleted();
             return View(new ArticleAddDto { Categories = categories });
+
         }
         [HttpGet]
         public async Task<IActionResult> Update(Guid articleId)
@@ -52,7 +70,17 @@ namespace Blog.Web.Areas.Admin.Controllers
         [HttpPost]
         public async Task<IActionResult> Update(ArticleUpdateDto articleUpdateDto)
         {
-            await articleService.UpdateArticleAsync(articleUpdateDto);
+            var map = mapper.Map<Article>(articleUpdateDto);
+            var result = await validator.ValidateAsync(map);
+
+            if (result.IsValid)
+            {
+                await articleService.UpdateArticleAsync(articleUpdateDto);
+            }
+            else
+            {
+                result.AddToModelState(this.ModelState);
+            }
 
             var categories = await categoryService.GetAllCategoriesNonDeleted();
 
