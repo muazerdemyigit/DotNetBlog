97e994f79d924516301a8e26bed154bf668bf1a1
muazerdemyigit <muazerdemyigit@hotmail.com>
2023-09-16 07:42:01 +0300

    Completed user management section accessible only to the superadmin role, listing all users along with their roles.

---
+++ Blog/Blog.Entity/DTOs/Users/UserDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Blog.Entity.DTOs.Users
+{
+    public class UserDto
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public bool EmailConfirmed { get; set; }
+        public string PhoneNumber { get; set; }
+        public int AccessFailedCount { get; set; }
+        public string Role { get; set; }
+
+    }
+}
---
+++ Blog/Blog.Service/AutoMapper/Users/UserProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Blog.Entity.DTOs.Users;
+using Blog.Entity.Entities;
+
+namespace Blog.Service.AutoMapper.Users
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<AppUser, UserDto>().ReverseMap();
+        }
+    }
+}
