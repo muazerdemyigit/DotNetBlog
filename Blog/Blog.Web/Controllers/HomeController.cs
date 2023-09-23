68d8665efb38da86ce920053c5281992572812a5
muazerdemyigit <muazerdemyigit@hotmail.com>
2023-09-23 06:04:27 +0300

    Created a structure to display the details of an article when clicked.

--- Blog/Blog.Web/Controllers/HomeController.cs
+++ Blog/Blog.Web/Controllers/HomeController.cs
@@ -38,5 +38,10 @@ namespace Blog.Web.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public async Task<IActionResult> Detail(Guid id)
+        {
+            var article = await articleService.GetArticleWithCategoryNonDeletedAsync(id);
+            return View(article);
+        }
     }
 }
