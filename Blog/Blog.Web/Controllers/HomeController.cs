1f57bfcc5eb920d3161df105f1681d5bfe01d8dd
muazerdemyigit <muazerdemyigit@hotmail.com>
2023-09-24 06:40:18 +0300

    Implemented filters - Article Click Count #2.

--- Blog/Blog.Web/Controllers/HomeController.cs
+++ Blog/Blog.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Blog.Service.Services.Abstractions;
+﻿using Blog.Data.UnitOfWorks;
+using Blog.Entity.Entities;
+using Blog.Service.Services.Abstractions;
 using Blog.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -9,11 +11,15 @@ namespace Blog.Web.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IArticleService articleService;
+        private readonly IHttpContextAccessor httpContextAccessor;
+        private readonly IUnitOfWork unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger, IArticleService articleService)
+        public HomeController(ILogger<HomeController> logger, IArticleService articleService, IHttpContextAccessor httpContextAccessor, IUnitOfWork unitOfWork)
         {
             _logger = logger;
             this.articleService = articleService;
+            this.httpContextAccessor = httpContextAccessor;
+            this.unitOfWork = unitOfWork;
         }
         [HttpGet]
         public async Task<IActionResult> Index(Guid? categoryId, int currentPage = 1, int pageSize = 3, bool isAscending = false)
@@ -40,8 +46,27 @@ namespace Blog.Web.Controllers
         }
         public async Task<IActionResult> Detail(Guid id)
         {
-            var article = await articleService.GetArticleWithCategoryNonDeletedAsync(id);
-            return View(article);
+            var ipAddress = httpContextAccessor.HttpContext.Connection.RemoteIpAddress.MapToIPv4().ToString();
+            var articleVisitors = await unitOfWork.GetRepository<ArticleVisitor>().GetAllAsync(null, x => x.Visitor, y => y.Article);
+            var article = await unitOfWork.GetRepository<Article>().GetAsync(x => x.Id == id);
+
+            var result = await articleService.GetArticleWithCategoryNonDeletedAsync(id);
+
+            var visitor = await unitOfWork.GetRepository<Visitor>().GetAsync(x => x.IpAddress == ipAddress);
+
+            var addArticleVisitors = new ArticleVisitor(article.Id, visitor.Id);
+
+            if (articleVisitors.Any(x => x.VisitorId == addArticleVisitors.VisitorId && x.ArticleId == addArticleVisitors.ArticleId))
+                return View(result);
+            else
+            {
+                await unitOfWork.GetRepository<ArticleVisitor>().AddAsync(addArticleVisitors);
+                article.ViewCount += 1;
+                await unitOfWork.GetRepository<Article>().UpdateAsync(article);
+                await unitOfWork.SaveAsync();
+            }
+
+            return View(result);
         }
     }
 }
